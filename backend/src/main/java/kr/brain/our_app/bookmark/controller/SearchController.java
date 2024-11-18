874468571a647ffe1a3ee52ec2c777f3d1925c8b
skyline <107971203+cosmolatte@users.noreply.github.com>
2024-11-18 17:42:21 +0900

    search controller (getmapping 으로 단순히 word가 들어오는 것만 추가함)

---
+++ backend/src/main/java/kr/brain/our_app/bookmark/controller/SearchController.java
@@ -0,0 +1,26 @@
+package kr.brain.our_app.bookmark.controller;
+
+
+import kr.brain.our_app.bookmark.dto.SearchDto;
+import kr.brain.our_app.bookmark.service.SearchService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/search")
+@CrossOrigin(origins = "chrome-extension://nmppigpifceknpehjphklpnpofimlmea/")  // Chrome 확장 프로그램의 origin 추가
+
+public class SearchController {
+
+    private final SearchService searchService;
+
+    public SearchController(SearchService searchService) {
+        this.searchService = searchService;
+    }
+
+    @GetMapping
+    public ResponseEntity<SearchDto> searchByWord(@RequestParam String word, @RequestParam String email) {
+        SearchDto searchResult = searchService.searchByWord(word, email);
+        return ResponseEntity.ok(searchResult);
+    }
+}
